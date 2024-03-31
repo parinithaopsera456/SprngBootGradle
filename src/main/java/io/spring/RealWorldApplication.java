25ac99d28d5b276af4d946b1483ea63720f76106
zhangbose <129360131+zhangbose@users.noreply.github.com>
2024-03-31 11:48:47 +0800

    Update RealWorldApplication.java
    test

--- src/main/java/io/spring/RealWorldApplication.java
+++ src/main/java/io/spring/RealWorldApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealWorldApplication {
+  hello
 
   public static void main(String[] args) {
     SpringApplication.run(RealWorldApplication.class, args);
